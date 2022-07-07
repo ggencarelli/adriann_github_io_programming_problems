a4e294616eef537e85c0e609879b996b4e492b1f
George Gencarelli <george.gencarelli@gmail.com>
2022-07-07 15:10:50 -0400

    lists_and_strings_01

---
+++ lists_and_strings/lists_and_strings_01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#----------------------------------------------------------------------------
+
+import random
+
+def build_list():
+    num_list = []
+    while True:
+        rand = random.randint(1, 1000)
+        num_list.append(rand)
+        if len(num_list) > 50:
+            break
+    return num_list
+
+def main():
+    high = max(build_list())
+    print("The highest number in my list is", high)
+
+if __name__ == "__main__":
+    main()
